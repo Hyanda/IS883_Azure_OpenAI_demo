9a2665870c8eee3aebbf42fa503f5fcb6896b2a8
Hyanda <44890850+Hyanda@users.noreply.github.com>
2023-10-16 17:08:30 -0400

    Added temperature

--- azure.py
+++ azure.py
@@ -9,11 +9,13 @@ openai.api_key = os.environ.get('OPENAI_API_KEY')
 st.title("CT WebBot")
 # Create a text input field for user queries
 user_input = st.text_input("Ask a question:")
+temperature = st.slider("Select Temperature", 0.1, 1.0, 0.7, 0.1)
 # Send the user's query to OpenAI GPT-3
 if user_input:
     response = openai.Completion.create(
     engine="text-davinci-003",
     prompt=user_input,
-    max_tokens=50
+    max_tokens=50,
+    temperature=temperature
     )
     st.write(response['choices'][0]['text'].strip())
